9c7ab164734456384d69bfa4b9061370cad93130
XPixelCoderX <xpixelcoderx@gmail.com>
2026-04-13 16:24:17 -0400

    Update gamemodes.js

--- gamemodes.js
+++ gamemodes.js
@@ -1,8 +1,9 @@
 export const gamemodes = [
 "overall",
-"fireballmace","fireballfight","sword","axe","bedfight","crystal",
-"crystaldiamond","shieldlessuhc","diamondsmp","smp","elytramace",
-"mace","op","jousting","elytraspear","spear","diamondpotion",
-"netheritepotion","gapple","bridges","nodebuff","boxing","sumo",
-"battlerush","pearlfight","skywars","stickfight","topfight","archer","sg"
+"fireballmace","fireballfight","sword","axe","bedfight",
+"crystal","crystaldiamond","shieldlessuhc","diamondsmp",
+"smp","elytramace","mace","op","jousting","elytraspear",
+"spear","diamondpotion","netheritepotion","gapple",
+"bridges","nodebuff","boxing","sumo","battlerush",
+"pearlfight","skywars","stickfight","topfight","archer","sg"
 ];
