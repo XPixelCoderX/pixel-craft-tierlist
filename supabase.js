a31bf09413c892466794a80e6fe31652ed556ed4
XPixelCoderX <xpixelcoderx@gmail.com>
2026-04-13 16:39:26 -0400

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,6 +1,7 @@
 import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
 
-const supabaseUrl = "https://iphmjgrhkrhubgqqpgac.supabase.co";
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlwaG1qZ3Joa3JodWJncXFwZ2FjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzYwOTkyOTEsImV4cCI6MjA5MTY3NTI5MX0.iQ4kpMqVEcPxZdkTYXlmP6f8r6M8GkkJtSzXB28c0oY";
+const SUPABASE_URL = "https://iphmjgrhkrhubgqqpgac.supabase.co";
 
-export const supabase = createClient(supabaseUrl, supabaseKey);
+const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlwaG1qZ3Joa3JodWJncXFwZ2FjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzYwOTkyOTEsImV4cCI6MjA5MTY3NTI5MX0.iQ4kpMqVEcPxZdkTYXlmP6f8r6M8GkkJtSzXB28c0oY";
+
+export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
