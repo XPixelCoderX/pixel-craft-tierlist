fc11d3855062cb09b7d17cf3e01a5a082fbc407c
XPixelCoderX <xpixelcoderx@gmail.com>
2026-04-13 17:08:01 -0400

    Refactor gamemodes export to use const
    Refactored gamemodes export to use a constant variable.

--- gamemodes.js
+++ gamemodes.js
@@ -1,9 +1,10 @@
-export const gamemodes = [
-"overall",
-"fireballmace","fireballfight","sword","axe","bedfight",
-"crystal","crystaldiamond","shieldlessuhc","diamondsmp",
-"smp","elytramace","mace","op","jousting","elytraspear",
-"spear","diamondpotion","netheritepotion","gapple",
-"bridges","nodebuff","boxing","sumo","battlerush",
-"pearlfight","skywars","stickfight","topfight","archer","sg"
+// gamemodes.js – all modes exported
+const gamemodes = [
+  "overall",
+  "fireballmace","fireballfight","sword","axe","bedfight",
+  "crystal","crystaldiamond","shieldlessuhc","diamondsmp",
+  "smp","elytramace","mace","op","jousting","elytraspear",
+  "spear","diamondpotion","netheritepotion","gapple",
+  "bridges","nodebuff","boxing","sumo","battlerush",
+  "pearlfight","skywars","stickfight","topfight","archer","sg"
 ];
