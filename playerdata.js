0c3fb7e8703378c57211aed40521016f256075c1
XPixelCoderX <xpixelcoderx@gmail.com>
2026-05-30 13:53:41 -0400

    Update playerdata.js

--- playerdata.js
+++ playerdata.js
@@ -1,62 +1,12 @@
 // playerdata.js
 window.defaultPlayers = [
   {
-    ign: "TechnoTitan",
+    ign: "n2ab",
     region: "NA",
-    tiers: "sword:ht1,pot:ht2,uhc:lt1,crystal:ht1,axe:ht2,smp:ht1,mace:lt2,fireballfight:ht3,bedfight:ht2,vanilla:ht1,netheriteop:ht2,diamondsmp:lt1"
+    tiers: "sword:ht1,pot:ht1,uhc:ht4,crystal:lt1,axe:ht5,smp:ht4,mace:ht4,fireballfight:ht4,bedfight:ht4,vanilla:ht4,netheriteop:ht4,diamondsmp:ht4"
   },
   {
-    ign: "PixelPvP",
-    region: "EU",
-    tiers: "sword:lt2,pot:ht1,uhc:ht2,crystal:lt1,axe:ht1,smp:ht2,mace:ht3,fireballfight:ht1,bedfight:ht1,vanilla:lt2,netheriteop:ht1,diamondsmp:ht2"
-  },
-  {
-    ign: "StormBreaker",
-    region: "NA",
-    tiers: "sword:ht2,pot:lt2,uhc:ht1,crystal:ht2,axe:lt2,smp:ht3,mace:ht1,fireballfight:ht2,bedfight:ht2,vanilla:ht2,netheriteop:lt1,diamondsmp:ht3"
-  },
-  {
-    ign: "FrostByte",
-    region: "EU",
-    tiers: "sword:lt1,pot:ht3,uhc:ht2,crystal:lt2,axe:ht1,smp:ht1,mace:ht2,fireballfight:lt1,bedfight:ht3,vanilla:ht1,netheriteop:ht3,diamondsmp:lt2"
-  },
-  {
-    ign: "EmberFury",
-    region: "AS",
-    tiers: "sword:ht2,pot:ht2,uhc:ht3,crystal:ht3,axe:ht2,smp:ht2,mace:ht2,fireballfight:ht2,bedfight:ht2,vanilla:ht2,netheriteop:ht2,diamondsmp:ht2"
-  },
-  {
-    ign: "ShadowClaw",
-    region: "SA",
-    tiers: "sword:lt2,pot:lt2,uhc:lt2,crystal:ht2,axe:lt2,smp:ht4,mace:ht3,fireballfight:ht3,bedfight:lt2,vanilla:ht3,netheriteop:lt2,diamondsmp:ht4"
-  },
-  {
-    ign: "VenomStrike",
-    region: "OCE",
-    tiers: "sword:ht3,pot:ht2,uhc:ht2,crystal:ht2,axe:ht3,smp:ht2,mace:ht2,fireballfight:ht2,bedfight:ht2,vanilla:ht2,netheriteop:ht3,diamondsmp:ht2"
-  },
-  {
-    ign: "RazorEdge",
-    region: "NA",
-    tiers: "sword:lt3,pot:ht3,uhc:ht3,crystal:lt3,axe:ht4,smp:ht5,mace:ht4,fireballfight:lt2,bedfight:ht3,vanilla:lt3,netheriteop:ht4,diamondsmp:lt3"
-  },
-  {
-    ign: "CrimsonFang",
-    region: "EU",
-    tiers: "sword:ht2,pot:lt1,uhc:ht1,crystal:ht2,axe:ht2,smp:ht2,mace:ht2,fireballfight:ht1,bedfight:ht2,vanilla:ht1,netheriteop:ht2,diamondsmp:ht1"
-  },
-  {
-    ign: "NebulaStar",
-    region: "AS",
-    tiers: "sword:lt1,pot:ht2,uhc:ht2,crystal:ht1,axe:ht1,smp:ht1,mace:lt2,fireballfight:ht2,bedfight:ht2,vanilla:ht1,netheriteop:ht2,diamondsmp:ht2"
-  },
-  {
-    ign: "IronWill",
-    region: "AF",
-    tiers: "sword:ht4,pot:ht4,uhc:ht4,crystal:lt4,axe:ht5,smp:ht4,mace:ht4,fireballfight:ht4,bedfight:ht4,vanilla:ht4,netheriteop:ht4,diamondsmp:ht4"
-  },
-  {
-    ign: "BlazingPhoenix",
+    ign: "eagly",
     region: "NA",
     tiers: "sword:ht2,pot:ht2,uhc:ht2,crystal:ht2,axe:ht2,smp:ht2,mace:ht2,fireballfight:ht2,bedfight:ht2,vanilla:ht2,netheriteop:ht2,diamondsmp:ht2"
   }
