6955a4219906438803c144b0d8d423cc4a5e4786
XPixelCoderX <xpixelcoderx@gmail.com>
2026-05-30 12:47:41 -0400

    Create playerdata.js

---
+++ playerdata.js
@@ -0,0 +1,16 @@
+const defaultPlayers = [
+{
+  ign: "Technoblade",
+  region: "NA",
+  title: "Combat King",
+  avatar: "https://mc-heads.net/avatar/Technoblade",
+  tiers: "sword:ht1,pot:ht1,uhc:lt1,crystal:ht2"
+},
+{
+  ign: "Dream",
+  region: "NA",
+  title: "Hunter",
+  avatar: "https://mc-heads.net/avatar/Dream",
+  tiers: "sword:lt1,uhc:ht1,axe:ht1"
+}
+];
