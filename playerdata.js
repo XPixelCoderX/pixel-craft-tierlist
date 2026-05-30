cfe1ab6f975bbec8e24f745899541bf88253a705
XPixelCoderX <xpixelcoderx@gmail.com>
2026-05-30 13:59:59 -0400

    Update playerdata.js

--- playerdata.js
+++ playerdata.js
@@ -3,11 +3,11 @@ window.defaultPlayers = [
   {
     ign: "n2ab",
     region: "NA",
-    tiers: "sword:ht1,pot:ht1,uhc:ht4,crystal:lt1,axe:ht5,smp:ht4,mace:ht4,fireballfight:ht4,bedfight:ht4,vanilla:ht4,netheriteop:ht4,diamondsmp:ht4"
+    tiers: "sword:lt2"
   },
   {
     ign: "eagly",
     region: "NA",
-    tiers: "sword:ht2,pot:ht2,uhc:ht2,crystal:ht2,axe:ht2,smp:ht2,mace:ht2,fireballfight:ht2,bedfight:ht2,vanilla:ht2,netheriteop:ht2,diamondsmp:ht2"
+    tiers: "crystal:lt4"
   }
 ];
