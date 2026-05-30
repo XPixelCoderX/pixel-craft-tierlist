09009cd306ddd6145a6703b0aade87562451b357
XPixelCoderX <xpixelcoderx@gmail.com>
2026-05-30 12:30:08 -0400

    Delete supabase.js

--- supabase.js
+++
@@ -1,9 +0,0 @@
-// supabase.js – Pixel Craft Tierlist
-(function() {
-  const SUPABASE_URL = 'https://iphmjgrhkrhubgqqpgac.supabase.co';
-  const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlwaG1qZ3Joa3JodWJncXFwZ2FjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzYwOTkyOTEsImV4cCI6MjA5MTY3NTI5MX0.iQ4kpMqVEcPxZdkTYXlmP6f8r6M8GkkJtSzXB28c0oY';
-
-  // Create Supabase client and attach to window
-  const client = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
-  window.pixelSupabase = client;
-})();
